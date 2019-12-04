0d840f90626704c99776fa154a017b5709f00711
Dima <Shonen1988@gmail.com>
2019-12-04 10:25:25 +0200

    Calculator 1

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Calc {
+    //Add new panel with main components
+    JPanel windowContent = new JPanel();
+    
+    JButton myButton = new JButton();
+    JFrame myFrame = new JFrame();
+}
